6b610e58ef04f5a0dd5b8f2e69d1b2e02218c7fd
treu <luis.medina@utp.edu.co>
2022-12-26 11:06:54 -0500

    EnemySystem

--- Assets/Scripts/IA/IA.cs
+++ Assets/Scripts/IA/IA.cs
@@ -7,15 +7,51 @@ public class IA : MonoBehaviour
 {
     // Start is called before the first frame update
     public NavMeshAgent enemy;
-    public Transform nodo;
+    public Transform[] nodo;
+    private int index = 0;
+
+    public bool followPlayer;
+    private GameObject player;
+    private float distanceToPlayer;
+    public float distanceToFollowPlayer = 10;
+    public float distanceToFollowPath = 2;
     void Start()
     {
-        enemy.destination = nodo.position;
+        enemy.destination = nodo[0].position;
+        player = FindObjectOfType<PlayerMovement>().gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if(distanceToPlayer <= distanceToFollowPlayer && followPlayer)
+        {
+            FollowPlayer();
+        }
+        else
+        {
+            EnemyPath();
+        }
+    }
+
+    public void EnemyPath()
+    {
+        enemy.destination = nodo[index].position;
+        if(Vector3.Distance(transform.position, nodo[index].position) <= distanceToFollowPath)
+        {
+            if(nodo[index] != nodo[nodo.Length - 1])
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+    }
+    public void FollowPlayer()
+    {
+        enemy.destination = player.transform.position;
     }
 }
