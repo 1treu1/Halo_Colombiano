32a995dbc014967cbcb271654b0ddfb67dd2b02a
Diaz666B <112578185+Diaz666B@users.noreply.github.com>
2022-12-28 15:49:24 -0500

    Radar-Barra de vida

--- Assets/Scripts/RadarScripts/Radar.cs
+++ Assets/Scripts/RadarScripts/Radar.cs
@@ -8,11 +8,15 @@ public class Radar : MonoBehaviour
     public int velRadar;
     public LayerMask mascara;
     public Transform Player;
+    public Transform punteroRadar;
+    public Transform camaraRadar;
 
     private void Update()
     {
         transform.position = Player.position;//el objeto que tenga esta script estara siguiendo al player
         transform.Rotate(0, velRadar * Time.deltaTime, 0);//Velocidad del radar que solo que aploca en el eje y de la rotación
+        punteroRadar.Rotate(0, 0, -velRadar * Time.deltaTime);
+        camaraRadar.position = new Vector3(Player.position.x, camaraRadar.position.y, Player.position.z);
 
         RaycastHit hit;//va a detectar a los enemigos
         if (Physics.Raycast(transform.position, transform.forward, out hit, distRadar, mascara))
