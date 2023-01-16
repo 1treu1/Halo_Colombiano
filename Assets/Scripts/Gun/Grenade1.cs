bc88c6aaa6659e377ead5b3c3f625932914f4aec
treu <luis.medina@utp.edu.co>
2023-01-15 23:27:02 -0500

    Todo listo para empezar a construir el juego

---
+++ Assets/Scripts/Gun/Grenade1.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade1 : MonoBehaviour
+{
+    public float delay = 3;
+    float countdown;
+    public float radius = 5;
+    public float explosionForce = 70;
+    bool exploded = false;
+    public GameObject explosionEffect;
+    public AudioSource audio;
+    public AudioClip soundGrenade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach(var rangeObjects in colliders)
+        {
+            IA ia = rangeObjects.GetComponent<IA>();
+            if(ia != null)
+            {
+                ia.GrenadeImpact();
+            }
+            Rigidbody rb = rangeObjects.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(explosionForce * 10, transform.position, radius);
+            }
+        }
+        exploded = false;
+        Destroy(gameObject, 0.8f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hola "+other);
+        if (other.CompareTag("Player"))
+        {
+            exploded = true;
+            audio.PlayOneShot(soundGrenade);
+            Debug.Log("Exploded");
+            Explode();
+        }
+    }
+  
+}
